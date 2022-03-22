c852fb509e4fecb90245749c1df96776d4663561
Phan Quang Quy <phanquangquy9a5@gmail.com>
2022-03-22 10:02:10 +0700

    update title 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Phan Quang Quý
+          Nguyễn Nhật Ánh
         </p>
         
       </header>
